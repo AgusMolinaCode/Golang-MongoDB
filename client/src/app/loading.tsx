b3474c489c108c8f1197bf77300680884d96c4e5
Agustin <am.motos@hotmail.com>
2024-11-22 21:12:19 -0300

    feat: remove unused Next.js build artifacts and configuration files

--- client/src/app/loading.tsx
+++
@@ -1,8 +0,0 @@
-export default function Loading() {
-    // You can add any UI inside Loading, including a Skeleton.
-    return (
-        <div className="flex items-center justify-center h-screen">
-            <p className="text-lg font-semibold text-center">Loading...</p>
-        </div>
-    )
-  }
